5f4a9f9c9caa844dbfd8a581cbe7fd54eedcd2f4
CristianFernando88 <gordyfernando@gmail.com>
2023-05-27 11:48:38 -0300

    Ultimo cambio entrega 27/05

--- scripts.js
+++ scripts.js
@@ -12,28 +12,29 @@ function calcular(){
 
     let descuento = 0;
     let total = 0;
-
-    switch (parseInt(categoria.value)){
-        case 1:
-            descuento = 200 * 0.8;
-            total = (200 - descuento)*cantidad.value;
-            textoResumen += " "+total;
-            resumen.innerHTML = textoResumen;
-            break;
-        case 2:
-            descuento = 200 * 0.5;
-            total = (200 - descuento)*cantidad.value;
-            textoResumen += " "+total;
-            resumen.innerHTML = textoResumen;
-            break;
-        case 3:
-            descuento = 200 * 0.15;
-            total = (200 - descuento)*cantidad.value;
-            textoResumen += " "+total;
-            resumen.innerHTML = textoResumen;
-            break;
+    if (validar()) {
+        switch (parseInt(categoria.value)){
+            case 1:
+                descuento = 200 * 0.8;
+                total = (200 - descuento)*cantidad.value;
+                textoResumen += " "+total;
+                resumen.innerHTML = textoResumen;
+                break;
+            case 2:
+                descuento = 200 * 0.5;
+                total = (200 - descuento)*cantidad.value;
+                textoResumen += " "+total;
+                resumen.innerHTML = textoResumen;
+                break;
+            case 3:
+                descuento = 200 * 0.15;
+                total = (200 - descuento)*cantidad.value;
+                textoResumen += " "+total;
+                resumen.innerHTML = textoResumen;
+                break;
+        }
     }
-
+    
     console.log(`Nombre: ${nombre.value}
     Apellido: ${apellido.value}
     Cantidad: ${cantidad.value}
@@ -43,7 +44,26 @@ function calcular(){
 function borrar(){
     formulario['nombre'].value = "";
     formulario['apellido'].value = "";
+    formulario['correo'].value = "";
     formulario['cantidad'].value = "";
-    formulario['categoria'].value = "";
+    formulario['categoria'].value = "Estudiante";
     document.getElementById('resumen').innerHTML = "Total a Pagar: $";   
+}
+
+function validar(){
+    for(elemento of formulario){
+        if(elemento.value == ""){
+            alert(`Complete el campo ${elemento.id}`);
+            elemento.className = "form-control fs-4 border border-danger border-2";
+            elemento.focus();
+            elemento.style.outline = "solid #FDA08B";
+            console.log(elemento.className);
+            return false
+        }
+    }
+    return true;
+}
+function restablecerInput(elemento){
+    elemento.className = "form-control fs-4";
+    elemento.style = "";
 }
